ce047f280dac2eb0a86fed30d2018dcc13dd94e5
Franz <franzeduardo10@gmail.com>
2017-08-08 20:58:47 -0300

    Imprimindo mensagem de HelloWorld

--- HelloWorld/src/br/edu/univas/si4/es2/HelloWorld/HelloWorld.java
+++ HelloWorld/src/br/edu/univas/si4/es2/HelloWorld/HelloWorld.java
@@ -3,7 +3,8 @@ package br.edu.univas.si4.es2.HelloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!!!");
 
 	}
 
